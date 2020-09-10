1ae2a50938ddbdb095e4554c6289a115dc452411
poonam <jpoonam779@gmail.com>
2020-09-10 13:00:33 +0530

    initial commit

---
+++ hiii.py
@@ -0,0 +1,4 @@
+n = int(input("Enter no rows:: "))
+for i in range(n):
+     print(" *"*n)
+print("")
---
+++ pali.py
@@ -0,0 +1,6 @@
+word = input("Enter word: ")
+re_word =word[::-1]
+if word ==re_word:
+    print("pal")
+else:
+    print("not pal")
